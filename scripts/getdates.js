c1e7326293adb83edb4c2c7469c3167d448c89af
CooperSandra <scooper1@byupathway.edu>
2025-09-16 22:48:38 -0400

    home page completed with changes added

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Insert current year in footer
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Insert last modified date in footer
+document.getElementById("lastModified").textContent = "Last Modification" + document.lastModified;
